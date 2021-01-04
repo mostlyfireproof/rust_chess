846fd8368a31f3335e8a83c66354601cdfea1d53
mostlyfireproof <iranoutofemailadressideas@gmail.com>
2021-01-04 14:00:07 -0500

    starting functions for the chessboard

--- src/board.rs
+++ src/board.rs
@@ -1,17 +1,18 @@
-use std::os::raw::c_void;
-
-struct Board {
-
+pub(crate) struct Board {
+    grid: [[Piece; 8]; 8],
 }
 
 impl Board {
     // Initializes the board and adds all the pieces to their proper spot
-    fn init_board(&self) -> Board {
+    pub fn init_board(&self) -> () {
+        println!("testing1");
+
         // need to implement this
     }
 
     // Prints out the board to the console
-    fn print(&self) -> c_void {
+    pub fn print(&self) -> () {
+        println!("testing2");
         // need to implement this
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 mod piece;
 mod board;
+use crate::board::Board;
+
 
 fn main() {
+    let mut main_board = Board;
+    main_board::init_board();
+    main_board::print_board();
+
     println!("Hello, world!");
 }
